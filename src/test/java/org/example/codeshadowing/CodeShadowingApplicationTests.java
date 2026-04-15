43c476b5615c36d58311b91ddf39eb83975aada9
sparta <sparta@spartaui-MacBookAir.local>
2026-04-15 14:34:33 +0900

    Initial commit

---
+++ src/test/java/org/example/codeshadowing/CodeShadowingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.codeshadowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeShadowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
